13b43c23cc8842a97ebb495e0f3a63d288671d2c
shinanaa <916457399@qq.com>
2020-03-09 15:12:27 +0800

    访问地址切换

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,9 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-const useUrl = 'l'
+const useUrl = 't'
 const url = {
-  t: 'http://192.168.1.127/', // 测试
+  t: 'http://www.netpaper.top:5000/', // 测试
   l: 'http://192.168.1.81/' // 真实
 }
 
